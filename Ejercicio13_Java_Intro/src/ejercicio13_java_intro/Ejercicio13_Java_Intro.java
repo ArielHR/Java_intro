ef258d52abc2faa18dacb860ca95cbd554dcbb79
ArielHR <arielhr_09@hotmail.com>
2023-03-06 19:18:48 -0600

    Ejercicio_13

--- Ejercicio13_Java_Intro/src/ejercicio13_java_intro/Ejercicio13_Java_Intro.java
+++ Ejercicio13_Java_Intro/src/ejercicio13_java_intro/Ejercicio13_Java_Intro.java
@@ -1,32 +1,40 @@
 /*
 Dibujar un cuadrado de N elementos por lado utilizando el carácter “*”. 
 Por ejemplo, si el cuadrado tiene 4 elementos por lado se deberá dibujar lo siguiente:
-* * ** **
-       
 
-** * * **
  */
 package ejercicio13_java_intro;
 
-import java.util.Scanner;
+import java.util.Scanner ; 
 
 public class Ejercicio13_Java_Intro {
 
 
     public static void main(String[] args) {
-        Scanner leer = new Scanner (System.in);
-        System.out.println("Ingresa el tamaño de los lados de la fgura deseada");
-        int lado = leer.nextInt();
-
-        for (int i = 0 ; i < lado ; i++) {
-            
-            for (int y = 0 ; i < lado ; i++) {
-                if ( i = 1 && i < lado) {
-                    System
-                }
-            }
-            System.out.println("");
-        }
+       Scanner leer = new Scanner (System.in);
+       int numU,a,b,y;
+       System.out.println("Ingresa el número de lado de la figura");
+       numU = leer.nextInt();
+       
+       for (a = 0; a < numU ; a++) {
+           System.out.print(" * ");
+       }
+       System.out.println();
+       
+       for (a= 0 ; a < numU - 2 ; a++){
+           System.out.print(" * ");
+           for (y=0 ; y < numU - 2 ; y++){
+               System.out.print("   ");
+           }
+           System.out.println(" * ");
+       }
+       
+       System.out.println();
+       
+       for (a = 0; a < numU ; a++) {
+           System.out.print(" * ");
+       }
+       
     }
     
 }
